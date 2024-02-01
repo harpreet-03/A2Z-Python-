86eb3c7fa08ae99a8adf2e31e48d67e88e6b19d1
Harpreet singh <harpreet162004@gmail.com>
2024-02-01 22:03:58 +0530

    initial commit

---
+++ Sets & Dictionary/Dict.py
@@ -0,0 +1,15 @@
+"""
+Dictionary: 
+
+- Dictionaries are used to store data values in key:value pairs
+- They are unordered, mutable(changeable) & don't allow duplicate keys
+
+"""
+
+dict = {
+    "name": "harpreet",
+    "cgpa": 7.88,
+    "marks": [88,77,71],
+}
+
+print("Dictionary: ", dict)
