936fa1db826548355d3d12932d2f9f85f2b14b5e
Harpreet singh <harpreet162004@gmail.com>
2024-02-19 13:00:32 +0530

    Files in py

--- File I_O/f1.py
+++ File I_O/f1.py
@@ -1,6 +1,34 @@
 f = open("demo.txt", "r")
+print(f.read())
+f.close()
+print("1")
+f = open("demo.txt", "r")
+print(f.read(5))
+f.close()
+print("2")
+f = open("demo.txt", "r")
+print(f.readline())
+f.close()
+print("3")
+f = open("demo.txt", "r")
+print(f.readline())
+print(f.readline())
+f.close()
+print("4")
 
-data = f.read() # read the whole file at once
-print(data)
+f = open("demo.txt", "r")
+for x in f:
+    print(x)
+f.close()  # Don't forget to close the file
+print(type(f))
 
-print(type(data))
+
+
+#or
+"""
+with open("demo.txt", "r") as f:
+    for x in f:
+        print(x)
+
+"""
+
